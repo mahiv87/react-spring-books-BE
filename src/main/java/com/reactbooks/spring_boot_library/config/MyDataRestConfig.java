d20ec5be98883f57679655fa21867360a24dc21e
mahiv87 <maherreraiv1987@gmail.com>
2024-06-23 09:15:56 -0600

    configured the spring rest default

---
+++ src/main/java/com/reactbooks/spring_boot_library/config/MyDataRestConfig.java
@@ -0,0 +1,41 @@
+package com.reactbooks.spring_boot_library.config;
+
+import com.reactbooks.spring_boot_library.model.Book;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private String allowedOrigins = "http://localhost:3000";
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] unsupportedActions = {
+                HttpMethod.POST,
+                HttpMethod.PATCH,
+                HttpMethod.PUT,
+                HttpMethod.DELETE
+        };
+
+        config.exposeIdsFor(Book.class);
+
+        disableHttpMethods(Book.class, config, unsupportedActions);
+
+        cors.addMapping(config.getBasePath() + "/**")
+                .allowedOrigins(allowedOrigins);
+
+    }
+
+    private void disableHttpMethods(Class modelClass,
+                                    RepositoryRestConfiguration config,
+                                    HttpMethod[] unsupportedActions) {
+        config.getExposureConfiguration()
+                .forDomainType(modelClass)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));
+    }
+}
