4138964d39d0fbbe90672285aa5972f4578904d4
mahiv87 <maherreraiv1987@gmail.com>
2024-06-25 20:29:08 -0600

    debugged cors

--- src/main/java/com/reactbooks/spring_boot_library/config/MyDataRestConfig.java
+++ src/main/java/com/reactbooks/spring_boot_library/config/MyDataRestConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
 
-    private String allowedOrigins = "http://localhost:3000";
+    private String allowedOrigins = "http://localhost:5173";
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
