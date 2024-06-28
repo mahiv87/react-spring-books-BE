d8344179944e4892581cf481a7e498d8412e8a0d
mahiv87 <maherreraiv1987@gmail.com>
2024-06-27 19:04:47 -0600

    added title search request to repository

--- src/main/java/com/reactbooks/spring_boot_library/repository/BookRepository.java
+++ src/main/java/com/reactbooks/spring_boot_library/repository/BookRepository.java
@@ -1,7 +1,11 @@
 package com.reactbooks.spring_boot_library.repository;
 
 import com.reactbooks.spring_boot_library.model.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+    Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
 }
