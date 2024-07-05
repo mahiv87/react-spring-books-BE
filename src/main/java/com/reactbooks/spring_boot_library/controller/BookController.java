495e7cde0db309a8ec1c8f2382379adfdef16d45
mahiv87 <maherreraiv1987@gmail.com>
2024-07-05 09:37:50 -0600

    book controller init

---
+++ src/main/java/com/reactbooks/spring_boot_library/controller/BookController.java
@@ -0,0 +1,25 @@
+package com.reactbooks.spring_boot_library.controller;
+
+import com.reactbooks.spring_boot_library.model.Book;
+import com.reactbooks.spring_boot_library.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:5173")
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+    private BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PutMapping("/secure/checkout")
+    public Book checkoutBook (@RequestParam Long bookId) throws Exception {
+        String userEmail = "testuser@email.com";
+        return bookService.checkoutBook(userEmail, bookId);
+    }
+
+}
