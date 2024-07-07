3838ce4160fd3c4ed5e56f4409376b1907c72a8d
mahiv87 <maherreraiv1987@gmail.com>
2024-07-07 08:38:22 -0600

    post review now working

--- src/main/java/com/reactbooks/spring_boot_library/controller/ReviewController.java
+++ src/main/java/com/reactbooks/spring_boot_library/controller/ReviewController.java
@@ -1,11 +1,31 @@
 package com.reactbooks.spring_boot_library.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.reactbooks.spring_boot_library.requestmodels.ReviewRequest;
+import com.reactbooks.spring_boot_library.service.ReviewService;
+import com.reactbooks.spring_boot_library.utils.ExtractJWT;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin("http://localhost:5173")
 @RestController
 @RequestMapping("/api/reviews")
 public class ReviewController {
+
+    private ReviewService reviewService;
+
+
+    public ReviewController(ReviewService reviewService) {
+        this.reviewService = reviewService;
+    }
+
+    @PostMapping("/secure")
+    public void postReview(@RequestHeader(value = "Authorization") String token, @RequestBody ReviewRequest reviewRequest) throws Exception {
+
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+
+        if (userEmail == null) {
+            throw new Exception("User email is missing");
+        }
+        reviewService.postReview(userEmail, reviewRequest);
+    }
+
 }
