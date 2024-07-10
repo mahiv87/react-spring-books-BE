55db44c1e8a779b5b3e38611139b46a25d1fe5b2
mahiv87 <maherreraiv1987@gmail.com>
2024-07-09 19:04:04 -0600

    Users loan history init

---
+++ src/main/java/com/reactbooks/spring_boot_library/model/History.java
@@ -0,0 +1,39 @@
+package com.reactbooks.spring_boot_library.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "History")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class History {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "user_email")
+    private String userEmail;
+
+    @Column(name = "checkout_date")
+    private String checkoutDate;
+
+    @Column(name = "return_date")
+    private String returnDate;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "img")
+    private String img;
+}
