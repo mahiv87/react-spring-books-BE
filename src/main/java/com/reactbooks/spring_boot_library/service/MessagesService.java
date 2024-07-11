f9419033c0fbd09a0642de0c77929276e0cf5392
mahiv87 <maherreraiv1987@gmail.com>
2024-07-10 18:54:31 -0600

    message service created

---
+++ src/main/java/com/reactbooks/spring_boot_library/service/MessagesService.java
@@ -0,0 +1,28 @@
+package com.reactbooks.spring_boot_library.service;
+
+import com.reactbooks.spring_boot_library.model.Message;
+import com.reactbooks.spring_boot_library.repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class MessagesService {
+
+    private MessageRepository messageRepository;
+
+    @Autowired
+    public MessagesService (MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+
+    public void postMessage (Message messageRequest, String userEmail) {
+        Message message = new Message(messageRequest.getTitle(), messageRequest.getQuestion());
+
+        message.setUserEmail(userEmail);
+        messageRepository.save(message);
+    }
+
+
+}
