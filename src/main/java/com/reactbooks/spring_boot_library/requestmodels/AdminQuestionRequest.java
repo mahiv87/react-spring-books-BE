b89f83e2659541e4ea8d237c19c214e0c092f700
mahiv87 <maherreraiv1987@gmail.com>
2024-07-15 12:16:18 -0600

    admin message request added to message service

---
+++ src/main/java/com/reactbooks/spring_boot_library/requestmodels/AdminQuestionRequest.java
@@ -0,0 +1,10 @@
+package com.reactbooks.spring_boot_library.requestmodels;
+
+
+import lombok.Data;
+
+@Data
+public class AdminQuestionRequest {
+    private Long id;
+    private String response;
+}
