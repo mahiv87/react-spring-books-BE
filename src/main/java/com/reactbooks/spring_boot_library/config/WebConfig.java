45498dca52b2947d27731383e81f44dde6ed66a6
mahiv87 <maherreraiv1987@gmail.com>
2024-07-27 10:50:58 -0600

    configure global cors

---
+++ src/main/java/com/reactbooks/spring_boot_library/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.reactbooks.spring_boot_library.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://reactspringlibrary.netlify.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
